44a1b3d45a4fa69eec98179ef0c484de559f79b1
rabia <tulurabia62@gmail.com>
2024-03-12 18:04:46 +0300

    new

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,12 +1,20 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route("/")
+def head():
+    return "hello word clarusway-17"
 
 
+@app.route("/second")
+def head2():
+    return "ahanda ikinci page"
 
-
+@app.route("/third")
+def head3():
+    return "buda 3 ncü sayfa"
 
 if __name__ == '__main__':
 
-    app.run(debug=True, port=30000)
-    # app.run(host= '0.0.0.0', port=8081)
+    #app.run(debug=True, port=30000)
+    app.run(host= '0.0.0.0', port=8081)
